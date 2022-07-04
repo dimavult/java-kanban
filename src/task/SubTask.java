2f4139a86d7d159514b884294b89a326cfd54bac
Дмитрий <yandjava@gmail.com>
2022-07-04 12:33:37 +0300

    feat: добавить конструктор

--- src/task/SubTask.java
+++ src/task/SubTask.java
@@ -1,26 +1,20 @@
-package Tasks;
+package task;
 
 import constants.TasksStatus;
 
 public class SubTask extends Task{
     protected int epicsId;
 
+    /*контруктор создан для метода по созданию сабтаски, так как нам нужно знать только epicId для создания*/
     public SubTask(String name, String description, TasksStatus.Status status, int epicsId) {
-
         super(name, description, status);
         this.epicsId = epicsId;
     }
 
-    public SubTask(int id, String name, String description, TasksStatus.Status status) {
+    public SubTask(String name, String description, TasksStatus.Status status, int id, int epicsId) {
         super(name, description, status, id);
+        this.epicsId = epicsId;
     }
-
-    //    public SubTask(String name, String description, TasksStatus.Status status, int id, int epicsId) {
-//        super(name, description, status, id);
-//        this.epicsId = epicsId;
-//    }
-
-
 
     public int getEpicsId() {
         return epicsId;
