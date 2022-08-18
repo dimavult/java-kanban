2a549f6249360951cc99fb9f7cadc7b363366f12
dimavult <yandjava@gmail.com>
2022-08-18 22:42:39 +0300

    change tests

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,16 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         FileBackedTasksManager manager = FileBackedTasksManager.loadFromFile(new File("data.txt"));
-
-        manager.addNewEpic(new Epic("--", "--"));
-        manager.addNewSubTask(new SubTask("--", "--", Status.NEW, 1));
-        manager.addNewSubTask(new SubTask("--", "--", Status.DONE, 1));
-        manager.addNewEpic(new Epic("--", "--"));
-        manager.addNewSubTask(new SubTask("--", "--", Status.DONE, 4));
-        manager.addNewSubTask(new SubTask("--", "--", Status.DONE, 4));
-        manager.addNewSubTask(new SubTask("--", "--", Status.NEW, 4));
-
+//        FileBackedTasksManager manager = new FileBackedTasksManager(new File("data.txt"));
+//
+//        manager.addNewEpic(new Epic("--", "--"));
+//        manager.addNewSubTask(new SubTask("--", "--", Status.NEW, 1));
+//        manager.addNewSubTask(new SubTask("--", "--", Status.DONE, 1));
+//        manager.addNewEpic(new Epic("--", "--"));
+//        manager.addNewSubTask(new SubTask("--", "--", Status.DONE, 4));
+//        manager.addNewSubTask(new SubTask("--", "--", Status.DONE, 4));
+//        manager.addNewSubTask(new SubTask("--", "--", Status.NEW, 4));
+//
 //        manager.getEpicById(1);
 //        manager.getEpicById(4);
 //        manager.getSubTaskById(2);
@@ -29,9 +30,7 @@ public class Main {
 //        manager.getSubTaskById(6);
 //        manager.getSubTaskById(7);
 
-//        manager.removeEpicByIdentifier(4);
-
-//        System.out.println(manager.getHistory());
+        System.out.println(manager.getHistory());
 //        System.out.println(manager.getEpicById(1));
 //        System.out.println(manager.getHistory());
     }
