fb6393b1a4ebb472915c26bef687258985d9c013
dimavult <yandjava@gmail.com>
2022-08-20 14:50:24 +0300

    refactor: change epic's constructor body

--- src/task/Epic.java
+++ src/task/Epic.java
@@ -22,6 +22,7 @@ public class Epic extends Task{
 
     public Epic(String name, String description, int id) {
         super(name, description, id);
+        this.status = Status.NEW;
     }
 
     @Override
